d89dc17acacb6cdc9df88ef4fd802b9f2cdc500e
Charan3500 <saicharansjp413@gmail.com>
2025-02-10 15:21:40 +0530

    fileadded

---
+++ SquareOfNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class SquareOfNumber
+{
+static void square()
+{
+Scanner scanner=new Scanner(System.in);
+System.out.print("enter the number");
+int num1=scanner.nextInt();
+System.out.print(num1*num1);
+}
+public static void main(String[] a)
+{
+square();
+}
+}
